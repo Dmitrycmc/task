b816ce6daac569d2d135656748bd62a4b6cee5c7
Dmitry Lytov <dmitrycmc@gmail.com>
2019-04-07 07:05:12 +0300

    Updates

---
+++ precache-manifest.65a7f9dfb5e2a25c672577a3bee21ca1.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "96a0977cb6b9a89b4278",
+    "url": "/telegram-chart/static/js/runtime~main.27d9b38b.js"
+  },
+  {
+    "revision": "11cfb7b79bf1129689bd",
+    "url": "/telegram-chart/static/js/main.3f7dc27d.chunk.js"
+  },
+  {
+    "revision": "c22293a9a390992d88d0c0f4aff8238b",
+    "url": "/telegram-chart/index.html"
+  }
+];
---
+++ static/js/main.3f7dc27d.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[function(n,o,p){n.exports=p(1)},function(n,o){alert("hi")}],[[0,1]]]);
+//# sourceMappingURL=main.3f7dc27d.chunk.js.map
