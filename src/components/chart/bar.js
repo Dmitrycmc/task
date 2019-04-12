9228fc409cd39237fb8bafc92b503da7e517ee9c
Dmitry Lytov <dmitrycmc@gmail.com>
2019-04-12 06:55:40 +0300

    Renders 5 charts

---
+++ src/components/chart/bar.js
@@ -0,0 +1,153 @@
+import { createSvgElement } from '../../helpers/elements';
+import { absToRel, interpolate } from '../../helpers/utils';
+
+const INTERSECTION_LINES_COLOR = 'gray';
+
+const generatePoints = (x, y) => {
+    const length = x.length;
+
+    let xMin = x[1],
+        dx = x[length - 1] - xMin;
+    let points = '' + 0 + ',' + y[1] + ' ';
+
+    for (let i = 2; i < length; i++) {
+        points += '' + (x[i] - xMin) / dx + ',' + y[i] + ' ';
+    }
+
+    return points;
+};
+
+const getLine = (xColumn, yColumn, stroke) => {
+    let visible = true;
+    const points = generatePoints(xColumn, yColumn);
+
+    const chartLine = createSvgElement(
+        'polyline',
+        { 'stroke-linejoin': 'round', 'vector-effect': 'non-scaling-stroke', points, stroke },
+        'chart-line'
+    );
+
+    const intersectionLineV = createSvgElement(
+        'line',
+        { x1: 0, x2: 0, y0: 0, y1: 1, 'vector-effect': 'non-scaling-stroke', stroke: INTERSECTION_LINES_COLOR },
+        'intersection-line'
+    );
+    const intersectionLineH = createSvgElement(
+        'line',
+        { x1: -1, x2: 1, y0: 0, y1: 0, 'vector-effect': 'non-scaling-stroke', stroke: INTERSECTION_LINES_COLOR },
+        'intersection-line'
+    );
+    const intersectionPoint0 = createSvgElement('circle', { r: 5, stroke, fill: 'white' }, 'intersection-point');
+    const intersectionPoint1 = createSvgElement('g', {}, '');
+    const intersectionPoint2 = createSvgElement('g', {}, 'animated');
+    const intersectionPoint3 = createSvgElement('g', {}, '');
+    const intersectionPoint4 = createSvgElement('g', {}, 'animated');
+    const intersectionPoint5 = createSvgElement('g', {}, '');
+    intersectionPoint1.appendChild(intersectionPoint0);
+    intersectionPoint2.appendChild(intersectionPoint1);
+    intersectionPoint3.appendChild(intersectionLineH);
+    intersectionPoint3.appendChild(intersectionPoint2);
+    intersectionPoint4.appendChild(intersectionPoint3);
+    intersectionPoint5.appendChild(intersectionLineV);
+    intersectionPoint5.appendChild(intersectionPoint4);
+
+    const mapLine = createSvgElement(
+        'polyline',
+        { 'stroke-linejoin': 'round', 'vector-effect': 'non-scaling-stroke', points, stroke },
+        'map-line'
+    );
+
+    const mapAreaYTransform = createSvgElement('g', {}, 'animated');
+    mapAreaYTransform.appendChild(mapLine);
+
+    const chartAreaYTransform = createSvgElement('g', {}, 'animated');
+    chartAreaYTransform.appendChild(chartLine);
+
+    const setYChartArea = (y0, y1) => {
+        chartAreaYTransform.setAttribute(
+            'transform',
+            `
+            scale(1 ${1 / (y1 - y0)}) 
+            translate(0 ${-y0})
+        `
+        );
+    };
+
+    const setYMapArea = (y0, y1) => {
+        mapAreaYTransform.setAttribute(
+            'transform',
+            `
+            scale(1 ${1 / (y1 - y0)}) 
+            translate(0 ${-y0})
+        `
+        );
+    };
+
+    const setIntersectionX = (xRel, x0, x1, y0, y1, svgW, svgH) => {
+        const x = absToRel(xRel, x0, x1);
+        if (x < 0 || x > 1) {
+            intersectionPoint5.style.display = 'none';
+            return;
+        }
+        intersectionPoint5.style.display = 'initial';
+
+        const y = interpolate(xColumn, yColumn, xRel);
+
+        intersectionPoint1.setAttribute('transform', `scale(${1 / svgW} ${1 / svgH})`);
+        intersectionPoint2.setAttribute('transform', `scale(1 ${y1 - y0})`);
+        intersectionPoint3.setAttribute('transform', `translate(0 ${y})`);
+        intersectionPoint4.setAttribute('transform', `scale(1 ${1 / (y1 - y0)}) translate(0 ${-y0})`);
+        intersectionPoint5.setAttribute('transform', `scale(${svgW} ${svgH}) translate(${x} 0) `);
+    };
+
+    const visibility = flag => {
+        if (flag === undefined) {
+            /* reading */
+            return visible;
+        } else {
+            /* writing */
+            visible = flag;
+            intersectionPoint0.setAttribute('stroke', flag ? stroke : 'transparent');
+            intersectionPoint0.setAttribute('fill', flag ? 'white' : 'transparent');
+            chartLine.setAttribute('stroke', flag ? stroke : 'transparent');
+            mapLine.setAttribute('stroke', flag ? stroke : 'transparent');
+            intersectionLineH.setAttribute('stroke', flag ? INTERSECTION_LINES_COLOR : 'transparent');
+        }
+    };
+
+    return {
+        chartLineNode: chartAreaYTransform,
+        setYChartArea,
+        mapLineNode: mapAreaYTransform,
+        setYMapArea,
+        visibility,
+        intersectionPoint: intersectionPoint5,
+        setIntersectionX
+    };
+};
+
+ (keys, xColumn, yColumns, colors) => {
+    const keysToLines = keys.reduce((obj, key) => {
+        const line = getLine(xColumn, yColumns[key], colors[key]);
+        return {
+            ...obj,
+            [key]: line
+        };
+    }, {});
+
+    return keysToLines;
+};
+
+export default class Bar {
+    constructor(parentNode) {
+
+    }
+
+    resize() {
+
+    }
+
+    render() {
+
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './global.css';
 import createChart from './components/chart/chart';
 import createFooter from './components/footer/footer';
-import data from './data/1/overview';
+import data1 from './data/1/overview';
+import data2 from './data/2/overview';
+import data3 from './data/3/overview';
+import data4 from './data/4/overview';
+import data5 from './data/5/overview';
 import { createElement } from './helpers/elements';
 
 const charts = createElement();
@@ -9,8 +13,35 @@ const root = document.getElementById('root');
 root.appendChild(charts);
 root.appendChild(createFooter());
 
-[data].forEach(chartData => {
+[data1].forEach(chartData => {
     const { node, init } = createChart(chartData, 'Chart #1');
+    charts.appendChild(node);
+    init();
+});
+
+[data2].forEach(chartData => {
+    const { node, init } = createChart(chartData, 'Chart #2');
+    charts.appendChild(node);
+    init();
+});
+
+
+[data3].forEach(chartData => {
+    const { node, init } = createChart(chartData, 'Chart #3');
+    charts.appendChild(node);
+    init();
+});
+
+
+[data4].forEach(chartData => {
+    const { node, init } = createChart(chartData, 'Chart #4');
+    charts.appendChild(node);
+    init();
+});
+
+
+[data5].forEach(chartData => {
+    const { node, init } = createChart(chartData, 'Chart #5');
     charts.appendChild(node);
     init();
 });
